a676d4e5e33b50e37c13858bdbc8f7731f27630a
Trương Mỹ Tâm <130839340+tam8738@users.noreply.github.com>
2025-04-05 13:48:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,45 @@
 import gradio as gr
-import numpy
+import numpy as np
 from PIL import Image
 from keras.models import load_model
-model = load_model('./model1_catsVSdogs.h5')
+import tensorflow as tf
+
+# Đảm bảo TensorFlow chỉ sử dụng CPU
+tf.config.set_visible_devices([], 'GPU')
+
+# Tải mô hình đã được huấn luyện
+model = load_model('./model.h5')
+
+# Các lớp hoa quả
 classes = { 
-    0:'its a cat',
-    1:'its a dog',
+    0: 'Apple',
+    1: 'Banana',
+    2: 'Grape',
+    3: 'Mango',
+    4: 'Strawberry',
 }
+
+# Hàm phân loại
 def classify(img):
     image = Image.open(img)
-    image = image.resize((128,128))
-    image = numpy.expand_dims(image, axis=0)
-    image = numpy.array(image)
-    image = image/255
-    pred = model.predict_classes([image])[0]
-    sign = classes[pred]
+    image = image.resize((100, 100))
+    image = np.expand_dims(image, axis=0)
+    image = np.array(image)
+    image = image / 255.0
+    pred = model.predict(image)[0]
+    sign = classes[np.argmax(pred)]
     print(sign)
     return sign
-img = "./dogs-vs-cats/test1/100.jpg"
+
+# Đường dẫn tới hình ảnh thử nghiệm
+img = "./path_to_your_test_image.jpg"
 classify(img)
 
+# Giao diện Gradio
+iface = gr.Interface(
+    fn=classify, 
+    inputs=gr.inputs.Image(shape=(100, 100)), 
+    outputs=gr.outputs.Textbox()
+)
 
-
-# iface = gr.Interface(classify, 
-#                     gr.inputs.Image(shape=(224, 224)),
-#                     gr.outputs.Textbox())
-
-# iface.launch(share=True)
+iface.launch(share=True)
